cdbd232f6c34223075e1370fe69e205078d1d725
voltamage <37029882+voltamage@users.noreply.github.com>
2023-08-06 07:52:11 +0000

    have full system update at beginning of init

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -Sy --noconfirm ansible git && git clone https://github.com/voltamage/dotfiles.git /home/artix/dotfiles && ansible-playbook /home/artix/dotfiles/local.yml --ask-vault-pass && cd /home/artix/dotfiles && git remote remove origin && git remote add origin git@github.com:voltamage/dotfiles.git && cd ~/ 
+sudo pacman -Syyu --noconfirm ansible git && git clone https://github.com/voltamage/dotfiles.git /home/artix/dotfiles && ansible-playbook /home/artix/dotfiles/local.yml --ask-vault-pass && cd /home/artix/dotfiles && git remote remove origin && git remote add origin git@github.com:voltamage/dotfiles.git && cd ~/ 
