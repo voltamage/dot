725a25d66e2a90020949664f9b2c707ae2dfb036
voltamage <37029882+voltamage@users.noreply.github.com>
2023-08-06 06:56:03 +0000

    added --noconfirm pacman arg to init script

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -Sy ansible git && git clone https://github.com/voltamage/dotfiles.git /home/artix/dotfiles && ansible-playbook /home/artix/dotfiles/local.yml --ask-vault-pass
+sudo pacman -Sy --noconfirm ansible git && git clone https://github.com/voltamage/dotfiles.git /home/artix/dotfiles && ansible-playbook /home/artix/dotfiles/local.yml --ask-vault-pass
