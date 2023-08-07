ca28e3733540994542fa7cbacc4948b2d3d6aa7b
voltamage <37029882+voltamage@users.noreply.github.com>
2023-08-07 01:23:46 +0000

    removed tmux from init

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo pacman -Syyu --noconfirm ansible git && git clone https://github.com/voltamage/dotfiles.git ~/dotfiles && ansible-playbook ~/dotfiles/local.yml --ask-vault-pass -v && cd ~/dotfiles && git remote remove origin && git remote add origin git@github.com:voltamage/dotfiles.git && tmux
+sudo pacman -Syyu --noconfirm ansible git && git clone https://github.com/voltamage/dotfiles.git ~/dotfiles && ansible-playbook ~/dotfiles/local.yml --ask-vault-pass -v && cd ~/dotfiles && git remote remove origin && git remote add origin git@github.com:voltamage/dotfiles.git && cd ~/
