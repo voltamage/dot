a77f6dd415ab6637ba06a40bf990812d009ac288
voltamage <37029882+voltamage@users.noreply.github.com>
2023-08-18 07:16:07 +0000

    initial wezterm config

---
+++ chezmoi/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+return {
+  color_scheme = 'Gruvbox Dark',
+  use_fancy_tab_bar = false,
+  warn_about_missing_glyphs = false,
+  window_background_opacity = 0.8,
+}
