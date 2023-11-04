8a9c54e4ac7858e6f5f8737eee5b12ae5553776c
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-04 06:49:19 +0000

    better organized settings

--- chezmoi/private_dot_librewolf/main/user.js
+++ chezmoi/private_dot_librewolf/main/user.js
@@ -1,6 +1,17 @@
 user_pref("browser.cache.disk.enable", false); // disable disk caching
 user_pref("browser.download.autohideButton", true); // hide download ui intil first dl
-user_pref("browser.startup.page", 3); // open previous windows and tabs
 user_pref("browser.toolbars.bookmarks.visibility", "never"); // hide bookmarks toolbar
 user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org"); // set dark ui
 user_pref("privacy.sanitize.sanitizeOnShutdown", false); // do not clear history on close
+
+// SETTINGS
+// General
+user_pref("browser.startup.page", 3); // Open previous windows and tabs
+
+// Tabs
+user_pref("browser.ctrlTab.sortByRecentlyUsed", true); // Ctrl+Tab cycles through tabs in recently used order
+user_pref("browser.link.open_newwindow", 3); // Open links in tabs instead of new windows
+user_pref("browser.tabs.loadInBackground", true); // When you open a link, image or media in a new tab, switch to it immediately
+user_pref("browser.tabs.warnOnClose", false); // Confirm before closing multiple tabs
+user_pref("browser.warnOnQuitShortcut", false); // Confirm before quitting with Ctrl+Q
+user_pref("privacy.userContext.enabled", true); // Enable Container Tabs
