64fb738f9ee0bda6fbaeb02946b63ad3b3ff5dd4
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-04 01:19:27 +0000

    browser autohide download but

--- chezmoi/private_dot_librewolf/main/user.js
+++ chezmoi/private_dot_librewolf/main/user.js
@@ -1,2 +1,3 @@
+user_pref("browser.download.autohideButton", true);
 user_pref("browser.toolbars.bookmarks.visibility", "never");
 user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
