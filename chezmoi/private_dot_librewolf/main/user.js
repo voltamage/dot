a3d35bca4fc50e0ebdfd3487e51e861043577527
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-04 04:37:21 +0000

    made user.js more verbose

--- chezmoi/private_dot_librewolf/main/user.js
+++ chezmoi/private_dot_librewolf/main/user.js
@@ -1,3 +1,6 @@
-user_pref("browser.download.autohideButton", true);
-user_pref("browser.toolbars.bookmarks.visibility", "never");
-user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org");
+user_pref("browser.cache.disk.enable", false); // disable disk caching
+user_pref("browser.download.autohideButton", true); // hide download ui intil first dl
+user_pref("browser.startup.page", 3); // open previous windows and tabs
+user_pref("browser.toolbars.bookmarks.visibility", "never"); // hide bookmarks toolbar
+user_pref("extensions.activeThemeID", "firefox-compact-dark@mozilla.org"); // set dark ui
+user_pref("privacy.sanitize.sanitizeOnShutdown", false); // do not clear history on close
