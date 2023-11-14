a89f8c62774569c243d3275e5ad0696bc3b66fe0
voltamage <37029882+voltamage@users.noreply.github.com>
2023-11-14 06:53:03 +0000

    caught up to sys update

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -140,11 +140,11 @@ require('lazy').setup({
       'williamboman/mason.nvim',
     },
   },
-  {
-    'ellisonleao/glow.nvim',
-    config = true,
-    cmd = 'Glow'
-  },
+  --{
+  --  'ellisonleao/glow.nvim',
+  --  config = true,
+  --  cmd = 'Glow'
+  --},
   -- {
   --   'xiyaowong/transparent.nvim',
   --   config = true,
